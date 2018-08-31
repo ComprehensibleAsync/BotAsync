71041144283c417253b9e94b2b85cc826fe4eaad
ComprehensibleAsync <42865352+ComprehensibleAsync@users.noreply.github.com>
2018-08-31 14:07:04 +0200

    Add Run.sh

---
+++ run.sh
@@ -0,0 +1,21 @@
+﻿#!/bin/bash
+trap 'break'; exit 0 SIGINT SIGTERM
+function git-try-pull() {
+    git pull --all
+}
+FAIL=0
+while true; do
+    if [ ${FAIL} -eq 4 ]; then
+        git-try-pull
+        let FAIL=0
+    fi
+        python .server.py
+        node .server.js
+        EXIT=${?}
+    if [ ${EXIT} -ne 0 ]; then
+         let FAIL=${FAIL}+1
+    else
+        let FAIL=0
+    fi
+        sleep 1
+done
